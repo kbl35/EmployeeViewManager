219e167121d404ace949eb2350e120d04dc273f0
kbl35 <kbl35@txstate.edu>
2019-10-30 00:50:47 -0500

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default{
+    apiKey: "AIzaSyCEEe3Z4tpL6GEEmDk0pFvvvDH8OyAWe4g",
+    authDomain: "ky-le-project2019.firebaseapp.com",
+    databaseURL: "https://ky-le-project2019.firebaseio.com",
+    projectId: "ky-le-project2019",
+    storageBucket: "ky-le-project2019.appspot.com",
+    messagingSenderId: "290253211920",
+    appId: "1:290253211920:web:a7f1263fdebd4c17ab3d95",
+    measurementId: "G-C5PR9FTSFB"
+}
